33c5c3cf96562887c1ca4dfb3c84b413f041465f
Piyush Kant <piyush@piyush.local>
2021-03-06 20:22:23 +0900

    add initial project

---
+++ TwitterX/TwitterXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterXApp.swift
+//  TwitterX
+//
+//  Created by Piyush Kant on 2021/03/06.
+//
+
+import SwiftUI
+
+@main
+struct TwitterXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
