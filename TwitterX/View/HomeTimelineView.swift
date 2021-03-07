b60106cdd5140e80fcaa06a33c8d2f76c6506109
Piyush Kant <piyush@piyush.local>
2021-03-07 18:54:54 +0900

    add infinite scrolling

--- TwitterX/View/HomeTimelineView.swift
+++ TwitterX/View/HomeTimelineView.swift
@@ -16,26 +16,53 @@ struct HomeTimelineView: View {
     }
     
     var body: some View {
-        let filter = NSLocalizedString("homeTimeline", comment: "")
+        let tweets: [Tweet] = self.homeTimelineViewModel.tweets
         
         NavigationView {
-            List {
-                Section(header: Text(filter).padding(.leading, -10)) {
-                    let tweets: [Tweet] = self.homeTimelineViewModel.tweets
-                    
-                    ForEach(tweets) { tweet in
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text(tweet.text)
-                                .frame(minHeight: 0, maxHeight: 100)
-                                .font(.title)
-                            
-                        }.padding()
-                    }
-                }.padding()
+            List(0..<tweets.count, id: \.self) { i in
+                if i == tweets.count - 1 {
+                    HomeTimelineCellView(tweet: tweets[i], isLast: true, homeTimelineViewModel: self.homeTimelineViewModel)
+                } else {
+                    HomeTimelineCellView(tweet: tweets[i], isLast: false, homeTimelineViewModel: self.homeTimelineViewModel)
+                }
             }.onAppear {
                 homeTimelineViewModel.fetchHomeTimeline(count: 10)
             }
+            .navigationBarTitle(Text(NSLocalizedString("homeTimeline", comment: "")))
+            .navigationBarItems(trailing:
+                                    Button("Settings") {}
+            )
+            .navigationBarBackButtonHidden(true)
+            .listStyle(PlainListStyle())
+        }
+    }
+}
+
+struct HomeTimelineCellView: View {
+    let tweet: Tweet
+    var isLast: Bool
+    @ObservedObject var homeTimelineViewModel: HomeTimelineViewModel
+    
+    init(tweet: Tweet, isLast: Bool, homeTimelineViewModel: HomeTimelineViewModel) {
+        self.tweet = tweet
+        self.isLast = isLast
+        self.homeTimelineViewModel = homeTimelineViewModel
+    }
+    
+    var body: some View {
+        let tweets: [Tweet] = self.homeTimelineViewModel.tweets
+        
+        VStack(alignment: .leading, spacing: 10) {
+            Text(tweet.text)
+                .frame(minHeight: 0, maxHeight: 100)
+                .font(.title)
             
-        }.navigationBarBackButtonHidden(true)
+            if (self.isLast) {
+                Text("").onAppear {
+                    self.homeTimelineViewModel.fetchHomeTimeline(count: tweets.count + 10)
+                }
+            }
+            
+        }.padding()
     }
 }
